d6f007fc966cab9357b5e6d2173c06c8a29391c5
ssrmm <ssrmm@users.noreply.github.com>
2022-08-29 13:31:32 +0200

    Create MWE

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Windows.UI.Notifications;
+
+namespace IconColorIInversion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ToastNotifierCompat? _notifier;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _notifier ??= ToastNotificationManagerCompat.CreateToastNotifier();
+
+            var builder = new ToastContentBuilder()
+                .AddText("Some tilte")
+                .AddText("Some description")
+                .SetToastDuration(ToastDuration.Short)
+                .SetToastScenario(ToastScenario.Default);
+
+            var notification = new ToastNotification(builder.GetXml());
+            _notifier.Show(notification);
+        }
+    }
+}
